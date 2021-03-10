13d4f57d34132a31113f8445018686cd3984c7d9
farepule <faruk.puska@gmail.com>
2021-03-11 00:25:07 +0100

    let's play

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,12 +2,9 @@ function main(){
     //Only change code below this line
     var x = 8;
     //Only change code above this line
-    returnx
+    return x;
 }
 
 console.log(main())
 
 module.exports = main;
-
-
-
