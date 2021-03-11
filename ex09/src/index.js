b46e6852103b0e2a2ad8110fce1c35a87f48a131
farepule <faruk.puska@gmail.com>
2021-03-11 10:34:42 +0100

    more cods are here :)

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    var difference = numOne - numTwo;
+
+    //Only change code above this line
+    return difference;
+
+}
+
+console.log(main(8, 5))
+
+module.exports = main;
